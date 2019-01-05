03dcc15db3e4ae111d069bd1aec8b5290f226263
zhuangshaochen <1570681442@qq.com>
2019-01-05 23:26:05 +0800

    用户管理页面完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,14 +1,56 @@
 $(function() {
+  var currentPage = 1;
+  var pageSize = 5;
+  rander();
+  // 获取数据渲染页面
+  function rander() {
     $.ajax({
-        type : "get",
-        url : "/user/queryUser",
+      type: "get",
+      url: "/user/queryUser",
+      data: {
+        page: currentPage,
+        pageSize: pageSize
+      },
+      dataType: "json",
+      success: function(res) {
+        // console.log(res);
+        var str = template("tmp", res);
+        $("tbody").html(str);
+        $("#paginator").bootstrapPaginator({
+          bootstrapMajorVersion: 3,
+          currentPage: currentPage,
+          totalPages: Math.ceil(res.total / res.size),
+          onPageClicked: function(a, b, c, page) {
+            currentPage = page;
+            rander();
+          }
+        });
+      }
+    });
+  }
+
+
+//   点击按钮改变用户状态
+  $('tbody').on('click',"#change-status",function() {
+    //   alert(1)
+    var id = $(this).parent().data("id");
+    var isDelete = $(this).parent().data('isdelete');
+    isDelete =  isDelete === 1 ? 0 : 1;
+    // console.log(id,isDelete);
+    $.ajax({
+        type : 'post',
+        url : "/user/updateUser",
         data : {
-            page : 1,
-            pageSize :5
+            id : id,
+            isDelete : isDelete
         },
         dataType : "json",
         success : function( res ) {
-            console.log(res)
+            // console.log(res);
+            if (res.success) {
+                rander();
+            }
         }
     })
-})
+  })
+});
